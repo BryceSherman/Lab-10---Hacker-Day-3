aafade3045e2a1b16d41f03d238052a82ad56854
Bryce Sherman <brycesherman123@gmail.com>
2026-03-30 14:40:16 -0500

    Update auth redirect URLs for Render frontend

--- frontend/src/authConfig.js
+++ frontend/src/authConfig.js
@@ -1,6 +1,6 @@
 export const authConfig = {
-  signInRedirectURL: "http://localhost:5173",
-  signOutRedirectURL: "http://localhost:5173",
+  signInRedirectURL: "https://lab-10-hacker-day-3.onrender.com",
+  signOutRedirectURL: "https://lab-10-hacker-day-3.onrender.com",
   clientID: "9cTiLnR6rnuGvULpF_mzHk9EZwUa",
   baseUrl: "https://api.asgardeo.io/t/bryce",
   scope: ["openid", "profile"]
